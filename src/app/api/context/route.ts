f50bcebe255b96550009c04e10c58480bf52d4d3
Nicolofranceschi <franceschinicolo@gmail.com>
2024-01-02 17:56:48 +0100

    Update package.json and add maxDuration constant

--- src/app/api/context/route.ts
+++ src/app/api/context/route.ts
@@ -1,6 +1,8 @@
 import { createEmbeddingsTable } from './insert'
 import { NextResponse } from 'next/server'
 
+export const maxDuration = 300; // This function can run for a maximum of 5 seconds
+
 export async function POST(req: Request) {
   const { url, pages } = await req.json()
   try {
